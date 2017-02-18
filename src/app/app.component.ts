df6924d74d06d31b85f2b8adbdb2aae3747712db
Manu SOdhi <mssodhi@ucdavis.edu>
2017-02-17 20:58:06 -0800

    routes fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ACTION } from './shared';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+
+import { ACTION } from './shared';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +13,21 @@ export class AppComponent implements OnInit {
 
   sideMenuItems = [
     {title: 'Projects', link: ''},
-    {title: 'Education', link: '/education'},
-    {title: 'Personal', link: '/personal'},
-    {title: 'Resume', link: '/resume'}
+    {title: 'Education', link: 'education'},
+    {title: 'Personal', link: 'personal'},
+    {title: 'Resume', link: 'resume'}
   ];
 
-  constructor(private store: Store<any>) {
+  constructor(private store: Store<any>, private router: Router) {
   }
 
   ngOnInit() {
     this.store.dispatch({ type: ACTION.LOAD_PROJECTS });
     this.store.dispatch({ type: ACTION.LOAD_COURSES });
+  }
+
+  navigateTo(link) {
+    this.router.navigateByUrl(link, { skipLocationChange: true });
   }
 
 }
